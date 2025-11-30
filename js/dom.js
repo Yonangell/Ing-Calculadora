7a8e21d52b988f9dceb83204bce20e9ec7803fae
Yonangell Guillen <121691054+Yonangell@users.noreply.github.com>
2025-11-30 18:37:26 +0100

    Agregando la presentacion al archivo dom.html y la logica al dom.js

---
+++ js/dom.js
@@ -0,0 +1,113 @@
+const numerosBtn = document.querySelectorAll("[data-number]");
+const operacionBtn = document.querySelectorAll("[data-operation]");
+const igualBtn = document.querySelector('[data-equals]');
+const eliminarBtn = document.querySelector('[data-delete]');
+const limpiarBtn = document.querySelector("[data-all-clear]");
+
+const operacionAnteriorTextoElemento = document.querySelector(
+  "[data-previous-operand]"
+);
+const operacionActualTextoElemento = document.querySelector(
+  "[data-current-operand]"
+);
+
+let operacionActual = "";
+let operacionAnterior = "";
+let operacion = undefined;
+
+function limpiar() {
+  operacionActual = "";
+  operacionAnterior = "";
+  operacion = undefined;
+}
+
+function eliminarNumero() {
+  operacionActual = operacionActual.toString().slice(0, -1);
+}
+
+function agregarNumero(numero) {
+  if (numero === "." && operacionActual.includes(".")) return;
+  operacionActual = operacionActual.toString() + numero.toString();
+}
+
+function escogerOperacion(seleccionarOperacion) {
+  if (operacionActual === "") return;
+
+  if (operacionActual !== "") {
+    calcular();
+  }
+
+  operacion = seleccionarOperacion;
+  operacionAnterior = operacionActual;
+  operacionActual = "";
+}
+
+function calcular() {
+  let calculando;
+
+  let anterior = parseFloat(operacionAnterior);
+  let continuar = parseFloat(operacionActual);
+
+  if (isNaN(anterior) || isNaN(continuar)) return;
+
+  switch (operacion) {
+    case "+":
+      calculando = anterior + continuar;
+      break;
+    case "-":
+      calculando = anterior - continuar;
+      break;
+    case "*":
+      calculando = anterior * continuar;
+      break;
+    case "/":
+      calculando = anterior / continuar;
+      break;
+
+    default:
+      return;
+  }
+
+  operacionActual = calculando;
+  operacion = undefined;
+  operacionAnterior = "";
+}
+
+function actualizar() {
+  operacionActualTextoElemento.textContent = operacionActual;
+
+  if (operacion != null) {
+    operacionAnteriorTextoElemento.textContent = `${operacionAnterior} ${operacion}`;
+  } else {
+    operacionAnteriorTextoElemento.textContent = "";
+  }
+}
+
+numerosBtn.forEach( boton => {
+  boton.addEventListener("click", () => {
+    agregarNumero(boton.textContent);
+    actualizar();
+  });
+});
+
+operacionBtn.forEach( boton => {
+  boton.addEventListener("click", () => {
+    escogerOperacion(boton.textContent);
+    actualizar();
+  });
+});
+
+igualBtn.addEventListener("click", button => {
+  calcular();
+  actualizar();
+});
+
+limpiarBtn.addEventListener("click", button => {
+  limpiar();
+  actualizar();
+});
+
+eliminarBtn.addEventListener("click", button => {
+  eliminarNumero();
+  actualizar();
+});
